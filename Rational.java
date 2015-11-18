266570dc62c1be82c966ffc633c40ee3a800cb91
James Wang <jameswang9909@hotmail.com>
2015-11-18 14:40:13 -0500

    PHASE 2 COMPLETE

--- Rational.java
+++ Rational.java
@@ -49,21 +49,51 @@ public class Rational {
 		denominator = denominator * r.numerator;
 	}
 
+	public void reduce() {
+	    int g = gcd(numerator, denominator);
+	    numerator /= g;
+	    denominator /= g;
+	}
+
+	public void add(Rational r) {
+	    int g = gcd(denominator, r.denominator);
+            denominator *= r.denominator / g;
+            numerator *= r.denominator / g;
+            numerator += (r.numerator * (denominator / r.denominator));
+	}
+
+	public void subtract(Rational r) {
+	    int g = gcd(denominator, r.denominator);
+            denominator *= r.denominator / g;
+            numerator *= r.denominator / g;
+            numerator -= (r.numerator * (denominator / r.denominator));
+	}
+
+        public static int gcd(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            return gcd(b, a%b);
+        }
 
 	// main method for testing
 	public static void main(String[] args) {
 		Rational r = new Rational(2, 3); // Stores the rational number 2/3
 		Rational s = new Rational(1, 2); // Stores the rational number 1/2
-		r.multiply(s); // Multiplies r by s, changes r to 2/6.  s remains 1/2
-		System.out.println(r.toString());
-		System.out.println(r.floatValue());
-		r.divide(s); // Divides r by s, changes r to 2/3.  s remains 1/2
-		System.out.println(r.toString());
-		System.out.println(r.floatValue());
-		Rational q = new Rational(); // tests default constructor
-		System.out.println(q.toString());
-		Rational x = new Rational(1, 0); // tests constructor, 0 as denominator
-		System.out.println(x.toString());
+		r.add(s);
+		System.out.println(r);
+		r.subtract(s);
+		System.out.println(r);
+		// r.multiply(s); // Multiplies r by s, changes r to 2/6.  s remains 1/2
+                // r.reduce();
+		// System.out.println(r.toString());
+		// System.out.println(r.floatValue());
+		// r.divide(s); // Divides r by s, changes r to 2/3.  s remains 1/2
+		// System.out.println(r.toString());
+		// System.out.println(r.floatValue());
+		// Rational q = new Rational(); // tests default constructor
+		// System.out.println(q.toString());
+		// Rational x = new Rational(1, 0); // tests constructor, 0 as denominator
 	}
 
 }
